7429bca714f3844f386ee70a3ef0fb04fd8f05e2
Praise Ebuka <praise.njoga@gmail.com>
2023-05-22 19:31:20 +0100

    first commit

---
+++ generate_token.php
@@ -0,0 +1,49 @@
+<?php
+
+// Get our helper functions
+require_once("inc/functions.php");
+
+// Set variables for our request
+$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$shared_secret = "TBB5wltKarRtKn5mUVZck9RxHePNN6Jo";
+$params = $_GET; // Retrieve all request parameters
+$hmac = $_GET['hmac']; // Retrieve HMAC request parameter
+
+$params = array_diff_key($params, array('hmac' => '')); // Remove hmac from params
+ksort($params); // Sort params lexographically
+
+$computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
+
+// Use hmac data to check that the response is from Shopify or not
+if (hash_equals($hmac, $computed_hmac)) {
+
+	// Set variables for our request
+	$query = array(
+		"client_id" => $api_key, // Your API key
+		"client_secret" => $shared_secret, // Your app credentials (secret key)
+		"code" => $params['code'] // Grab the access key from the URL
+	);
+
+	// Generate access token URL
+	$access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
+
+	// Configure curl client and execute request
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $access_token_url);
+	curl_setopt($ch, CURLOPT_POST, count($query));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	// Store the access token
+	$result = json_decode($result, true);
+	$access_token = $result['access_token'];
+
+	// Show the access token (don't do this in production!)
+	echo $access_token;
+
+} else {
+	// Someone is trying to be shady!
+	die('This request is NOT from Shopify!');
+}
